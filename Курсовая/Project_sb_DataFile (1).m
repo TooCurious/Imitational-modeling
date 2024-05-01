8dc61e85e36d69c95db16207b212ee35e4a8e9ce
Michael Vesnin <vesmih2023@gmail.com>
2024-05-01 23:09:52 +0300

    test
    test

---
+++ Курсовая/Project_sb_DataFile (1).m
@@ -0,0 +1,287 @@
+% Simscape(TM) Multibody(TM) version: 7.3
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(25).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(25).angle = 0.0;
+smiData.RigidTransform(25).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(25).ID = '';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [0 -15.000000000000069 0];  % mm
+smiData.RigidTransform(1).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(1).axis = [0.57735026918962584 0.57735026918962573 0.57735026918962573];
+smiData.RigidTransform(1).ID = 'B[link2.stp-2:-:Tire.stp-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [1.0701473041052623e-10 24.999999999999975 375.80266547879495];  % mm
+smiData.RigidTransform(2).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(2).axis = [0.57735026918962573 -0.57735026918962562 0.57735026918962573];
+smiData.RigidTransform(2).ID = 'F[link2.stp-2:-:Tire.stp-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [0 -15.000000000000069 0];  % mm
+smiData.RigidTransform(3).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(3).axis = [0.57735026918962584 0.57735026918962573 0.57735026918962573];
+smiData.RigidTransform(3).ID = 'B[link2.stp-3:-:Tire.stp-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [325.73040951325669 24.999999999999993 187.43980052354445];  % mm
+smiData.RigidTransform(4).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(4).axis = [-0.57735026918962562 -0.57735026918962584 -0.57735026918962595];
+smiData.RigidTransform(4).ID = 'F[link2.stp-3:-:Tire.stp-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [7.5754940515038172e-11 0 375.80266547879523];  % mm
+smiData.RigidTransform(5).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(5).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(5).ID = 'B[Tire.stp-5:-:link2.stp-5]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [24.999999999999993 -15.000000000020524 -2.6762592142404174e-11];  % mm
+smiData.RigidTransform(6).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(6).axis = [-0.5773502691896254 -0.57735026918962573 0.57735026918962618];
+smiData.RigidTransform(6).ID = 'F[Tire.stp-5:-:link2.stp-5]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [325.7304095132892 50 187.43980052348815];  % mm
+smiData.RigidTransform(7).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(7).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(7).ID = 'B[Tire.stp-5:-:link2.stp-6]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [25.000000000000007 -15.000000000005596 -7.4127370908172452e-12];  % mm
+smiData.RigidTransform(8).angle = 2.0943951023931962;  % rad
+smiData.RigidTransform(8).axis = [-0.57735026918962573 -0.57735026918962595 0.57735026918962551];
+smiData.RigidTransform(8).ID = 'F[Tire.stp-5:-:link2.stp-6]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [325.7304095132892 50 187.43980052348815];  % mm
+smiData.RigidTransform(9).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(9).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(9).ID = 'B[Tire.stp-6:-:link2.stp-7]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [24.999999999999972 -14.999999999993317 8.5620399659092072e-12];  % mm
+smiData.RigidTransform(10).angle = 2.0943951023931939;  % rad
+smiData.RigidTransform(10).axis = [-0.57735026918962606 -0.57735026918962518 0.57735026918962618];
+smiData.RigidTransform(10).ID = 'F[Tire.stp-6:-:link2.stp-7]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [7.5767571470347628e-11 0 375.80266547879518];  % mm
+smiData.RigidTransform(11).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(11).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(11).ID = 'B[Tire.stp-6:-:link2.stp-8]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [24.999999999999986 -14.999999999998048 2.460698311779197e-12];  % mm
+smiData.RigidTransform(12).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(12).axis = [-0.57735026918962606 -0.57735026918962584 0.57735026918962551];
+smiData.RigidTransform(12).ID = 'F[Tire.stp-6:-:link2.stp-8]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(13).translation = [202.140974278242 25 116.70614591379601];  % mm
+smiData.RigidTransform(13).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(13).axis = [-0.57735026918962584 -0.57735026918962573 -0.57735026918962573];
+smiData.RigidTransform(13).ID = 'B[Wheel.stp-1:-:link2.stp-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(14).translation = [-8.5265128291212022e-14 -343.61999999994595 -7.617018127348274e-12];  % mm
+smiData.RigidTransform(14).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(14).axis = [-0.57735026918962595 -0.57735026918962584 0.57735026918962551];
+smiData.RigidTransform(14).ID = 'F[Wheel.stp-1:-:link2.stp-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(15).translation = [0 25 233.41229182759298];  % mm
+smiData.RigidTransform(15).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(15).axis = [0.57735026918962584 -0.57735026918962573 0.57735026918962573];
+smiData.RigidTransform(15).ID = 'B[Wheel.stp-1:-:link2.stp-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(16).translation = [5.6843418860808015e-14 -343.61999999999506 -6.8212102632969618e-13];  % mm
+smiData.RigidTransform(16).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(16).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(16).ID = 'F[Wheel.stp-1:-:link2.stp-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(17).translation = [-202.140974278242 25 116.70614591379601];  % mm
+smiData.RigidTransform(17).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(17).axis = [-0.57735026918962584 -0.57735026918962573 -0.57735026918962573];
+smiData.RigidTransform(17).ID = 'B[Wheel.stp-1:-:link2.stp-5]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(18).translation = [2.5579538487363607e-13 -343.6199999999987 -1.7053025658242404e-13];  % mm
+smiData.RigidTransform(18).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(18).axis = [-0.5773502691896254 -0.57735026918962584 0.57735026918962618];
+smiData.RigidTransform(18).ID = 'F[Wheel.stp-1:-:link2.stp-5]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(19).translation = [-202.140974278242 25 -116.70614591379601];  % mm
+smiData.RigidTransform(19).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(19).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(19).ID = 'B[Wheel.stp-1:-:link2.stp-6]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(20).translation = [-8.5265128291212022e-14 -343.62000000002803 4.2064129956997931e-12];  % mm
+smiData.RigidTransform(20).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(20).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(20).ID = 'F[Wheel.stp-1:-:link2.stp-6]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(21).translation = [202.140974278242 25 -116.70614591379601];  % mm
+smiData.RigidTransform(21).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(21).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(21).ID = 'B[Wheel.stp-1:-:link2.stp-7]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(22).translation = [-5.6843418860808015e-13 -343.62000000000018 -1.7053025658242404e-13];  % mm
+smiData.RigidTransform(22).angle = 2.0943951023931935;  % rad
+smiData.RigidTransform(22).axis = [-0.57735026918962595 -0.57735026918962518 0.57735026918962618];
+smiData.RigidTransform(22).ID = 'F[Wheel.stp-1:-:link2.stp-7]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(23).translation = [0 25 -233.41229182759298];  % mm
+smiData.RigidTransform(23).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(23).axis = [-0.57735026918962584 -0.57735026918962573 -0.57735026918962573];
+smiData.RigidTransform(23).ID = 'B[Wheel.stp-1:-:link2.stp-8]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(24).translation = [-1.7053025658242404e-13 -343.61999999999165 -1.1581846592889633e-12];  % mm
+smiData.RigidTransform(24).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(24).axis = [-0.57735026918962606 -0.57735026918962584 0.57735026918962529];
+smiData.RigidTransform(24).ID = 'F[Wheel.stp-1:-:link2.stp-8]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(25).translation = [0 0 0];  % mm
+smiData.RigidTransform(25).angle = 0;  % rad
+smiData.RigidTransform(25).axis = [0 0 0];
+smiData.RigidTransform(25).ID = 'RootGround[Wheel.stp-1]';
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(3).mass = 0.0;
+smiData.Solid(3).CoM = [0.0 0.0 0.0];
+smiData.Solid(3).MoI = [0.0 0.0 0.0];
+smiData.Solid(3).PoI = [0.0 0.0 0.0];
+smiData.Solid(3).color = [0.0 0.0 0.0];
+smiData.Solid(3).opacity = 0.0;
+smiData.Solid(3).ID = '';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.22199148163397445;  % kg
+smiData.Solid(1).CoM = [11.860955014041618 -187.71011853142576 0];  % mm
+smiData.Solid(1).MoI = [2330.639775952628 26.458061754121527 2334.2176409379595];  % kg*mm^2
+smiData.Solid(1).PoI = [0 0 -22.117772305068318];  % kg*mm^2
+smiData.Solid(1).color = [0.56470588235294117 0.56470588235294117 0.56470588235294117];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = 'link2.stp*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.22821677761052619;  % kg
+smiData.Solid(2).CoM = [164.11116865127343 25.000576734114777 284.1410299316496];  % mm
+smiData.Solid(2).MoI = [2987.5247233777113 10535.363516371692 7642.9291168465379];  % kg*mm^2
+smiData.Solid(2).PoI = [-45.691878088846856 4034.7672750067231 79.016575155558613];  % kg*mm^2
+smiData.Solid(2).color = [0.56470588235294117 0.56470588235294117 0.56470588235294117];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = 'Tire.stp*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 1.0982319677157331;  % kg
+smiData.Solid(3).CoM = [1.2633237595983115e-12 24.999999999998582 0];  % mm
+smiData.Solid(3).MoI = [8875.2167792663804 17292.836905317803 8875.2167792663095];  % kg*mm^2
+smiData.Solid(3).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(3).color = [0.56470588235294117 0.56470588235294117 0.56470588235294117];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = 'Wheel.stp*:*Default';
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(12).Rz.Pos = 0.0;
+smiData.RevoluteJoint(12).ID = '';
+
+smiData.RevoluteJoint(1).Rz.Pos = -37.993982068203792;  % deg
+smiData.RevoluteJoint(1).ID = '[link2.stp-2:-:Tire.stp-3]';
+
+%This joint has been chosen as a cut joint. Simscape Multibody treats cut joints as algebraic constraints to solve closed kinematic loops. The imported model does not use the state target data for this joint.
+smiData.RevoluteJoint(2).Rz.Pos = -98.146841956627142;  % deg
+smiData.RevoluteJoint(2).ID = '[link2.stp-3:-:Tire.stp-3]';
+
+smiData.RevoluteJoint(3).Rz.Pos = -38.176633907212668;  % deg
+smiData.RevoluteJoint(3).ID = '[Tire.stp-5:-:link2.stp-5]';
+
+%This joint has been chosen as a cut joint. Simscape Multibody treats cut joints as algebraic constraints to solve closed kinematic loops. The imported model does not use the state target data for this joint.
+smiData.RevoluteJoint(4).Rz.Pos = -97.962777592253971;  % deg
+smiData.RevoluteJoint(4).ID = '[Tire.stp-5:-:link2.stp-6]';
+
+smiData.RevoluteJoint(5).Rz.Pos = -98.176007115937466;  % deg
+smiData.RevoluteJoint(5).ID = '[Tire.stp-6:-:link2.stp-7]';
+
+%This joint has been chosen as a cut joint. Simscape Multibody treats cut joints as algebraic constraints to solve closed kinematic loops. The imported model does not use the state target data for this joint.
+smiData.RevoluteJoint(6).Rz.Pos = -37.96460746009015;  % deg
+smiData.RevoluteJoint(6).ID = '[Tire.stp-6:-:link2.stp-8]';
+
+smiData.RevoluteJoint(7).Rz.Pos = -38.129351543808767;  % deg
+smiData.RevoluteJoint(7).ID = '[Wheel.stp-1:-:link2.stp-2]';
+
+smiData.RevoluteJoint(8).Rz.Pos = -98.011472481022139;  % deg
+smiData.RevoluteJoint(8).ID = '[Wheel.stp-1:-:link2.stp-3]';
+
+smiData.RevoluteJoint(9).Rz.Pos = -157.83306239533002;  % deg
+smiData.RevoluteJoint(9).ID = '[Wheel.stp-1:-:link2.stp-5]';
+
+smiData.RevoluteJoint(10).Rz.Pos = 21.693650895863374;  % deg
+smiData.RevoluteJoint(10).ID = '[Wheel.stp-1:-:link2.stp-6]';
+
+smiData.RevoluteJoint(11).Rz.Pos = 142.03581679636787;  % deg
+smiData.RevoluteJoint(11).ID = '[Wheel.stp-1:-:link2.stp-7]';
+
+smiData.RevoluteJoint(12).Rz.Pos = 81.823568627604544;  % deg
+smiData.RevoluteJoint(12).ID = '[Wheel.stp-1:-:link2.stp-8]';
+
